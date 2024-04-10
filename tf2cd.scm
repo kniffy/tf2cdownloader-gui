59455b6f0023ef5e5dc7fc745b5c3a3e58f98513
kniffy <kniffy@sampler.fi>
2024-04-10 10:15:52 -0500

    some buttons

--- tf2cd.scm
+++ tf2cd.scm
@@ -1,10 +1,31 @@
-(import r7rs
-	pstk)
+(import pstk)
 
-(tk-start "tclsh")
-;(tk-start)
+; parse libraryfolders.vdf ?
+
+(tk-start) ; default calls tclsh8.6
 (ttk-map-widgets 'all) ; Use the Ttk widget set
-
 (tk/wm 'title tk "tf2cdownloader")
+(tk 'configure 'height: 600 'width: 800)
+
+(define statusbox
+  (tk 'create-widget 'label
+      'text: "this is a label"))
+
+(tk/pack
+  (tk 'create-widget 'button
+      'text: (quote "New Install")
+      #:command (lambda () (print "clicked install")))
+  (tk 'create-widget 'button
+      'text: 'Upgrade
+      #:command (lambda () (print "clicked upgrade")))
+  (tk 'create-widget 'button
+      'text: 'Quit
+      #:command (lambda () (tk-end)))
+  (tk 'create-widget 'label
+      'text: "some text"
+      'anchor: 'center)
+
+  #:expand #t
+  #:fill 'both)
 
 (tk-event-loop)
