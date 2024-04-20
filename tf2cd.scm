47dae58313b16c79b4cc99a7abc47d3afc29de47
kniffy <kniffy@sampler.fi>
2024-04-20 01:59:39 -0500

    file dialog nearly working

--- tf2cd.scm
+++ tf2cd.scm
@@ -1,3 +1,4 @@
+(import (chicken port))
 (import pstk)
 
 ; parse libraryfolders.vdf to autocomplete sourcemods dir?
@@ -11,6 +12,7 @@
 ; only initialize
 (tk-set-var! 'userdir "")
 
+; widget definitions
 (define label (tk 'create-widget 'label
 		  'text: "sourcemods dir:"))
 
@@ -19,11 +21,24 @@
 		  'textvariable: (tk-var 'userdir)))
 
 (define button (tk 'create-widget 'button
-		   'text: "button"))
+		   'text: "button"
+		   'command: (lambda ()
+			       (let ((cd (tk/choose-directory 'initialdir: "/tmp" 'mustexist: 'true)))
+				 (call-with-input-string
+				   cd
+				   (lambda (input-port)
+				     (tk-set-var! 'userdir input-port)))))))
 
-(tk/grid label 'row: 0 'column: 0 'columnspan: 3)
-(tk/grid entry 'row: 1 'column: 0 'columnspan: 3 'sticky: 'ew 'padx: 10 'pady: 10)
+(define spacerx (tk 'create-widget 'frame
+		   'width: 400))
+(define spacery (tk 'create-widget 'frame
+		    'height: 400))
+
+; actually drawing the window and placing positions
+(tk/grid label 'row: 0 'column: 0 'columnspan: 4 'padx: 10)
+(tk/grid entry 'row: 1 'column: 0 'columnspan: 7 'sticky: 'ew 'padx: 10 'pady: 10)
 (entry 'insert 0 "...steamapps/sourcemods")
-(tk/grid button 'row: 1 'column: 4 'padx: 10)
+(tk/grid button 'row: 1 'column: 8 'padx: 10)
+(tk/grid spacerx 'row: 2 'column: 0 'columnspan: 8)	; force the width
 
 (tk-event-loop)
