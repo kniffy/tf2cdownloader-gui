5267e5fcde0b3f1ead0866e6e5ecc8ea0856d158
kniffy <kniffy@sampler.fi>
2024-04-21 00:42:39 -0500

    awaiting more widgets

--- tf2cd.scm
+++ tf2cd.scm
@@ -13,14 +13,14 @@
 
 ; widget definitions
 (define label (tk 'create-widget 'label
-		  'text: "sourcemods dir:"))
+		  'text: "sourcemods directory:"))
 
 (define entry (tk 'create-widget 'entry
 		  'text: (tk-get-var 'userdir)
 		  'textvariable: (tk-var 'userdir)))
 
 (define button (tk 'create-widget 'button
-		   'text: "button"
+		   'text: "Browse"
 		   'command: (lambda ()
 			       (let ((cd (tk/choose-directory 'initialdir: "/tmp" 'mustexist: 'true)))
 				 (tk-set-var! 'userdir cd)))))
@@ -31,10 +31,10 @@
 		    'height: 400))
 
 ; actually drawing the window and placing positions
-(tk/grid label 'row: 0 'column: 0 'columnspan: 4 'padx: 10)
-(tk/grid entry 'row: 1 'column: 0 'columnspan: 7 'sticky: 'ew 'padx: 10 'pady: 10)
-(entry 'insert 0 "...steamapps/sourcemods")
+(tk/grid label 'row: 0 'column: 0 'columnspan: 4 'padx: 10 'pady: 10)
+(tk/grid entry 'row: 1 'column: 0 'columnspan: 7 'sticky: 'ew 'padx: 10)
+(entry 'insert 0 "Steam/steamapps/sourcemods")
 (tk/grid button 'row: 1 'column: 8 'padx: 10)
-(tk/grid spacerx 'row: 2 'column: 0 'columnspan: 8)	; force the width
+(tk/grid spacerx 'row: 2 'column: 0 'columnspan: 8 'pady: 5)	; force the width
 
 (tk-event-loop)
