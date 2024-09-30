7a35cf7897d68bee84429f329133891d784267ac
kniffy <kniffy+github@sampler.fi>
2024-09-30 08:50:52 -0500

    rebase curl to latest master

--- tf2cd.scm
+++ tf2cd.scm
@@ -15,7 +15,7 @@
 ; but for cursed reasons some of them are below, under
 ; the tk gui block, tk is a bitch with passing vars
 
-(define *downloader* (make-pathname "bin" "aria2c"))
+;(define *downloader* (make-pathname "bin" "aria2c"))
 (define *butler* (make-pathname "bin" "butler"))
 (define *curl* (make-pathname "bin" "curl")) ; vendored curl for http3
 (define *tar* "tar")
@@ -24,16 +24,16 @@
 ; set some platform-specific stuff
 (cond-expand
   (windows
-   (set! *downloader* (pathname-replace-extension *downloader* "exe"))
-   (set! *butler* (pathname-replace-extension *butler* "exe"))
-   (set! *curl* (pathname-replace-extension *curl* "exe"))
-   (set! *ttccll* (pathname-replace-extension *ttccll* "exe"))
+;    (set! *downloader* (pathname-replace-extension *downloader* "exe"))
+    (set! *butler* (pathname-replace-extension *butler* "exe"))
+    (set! *curl* (pathname-replace-extension *curl* "exe"))
+    (set! *ttccll* (pathname-replace-extension *ttccll* "exe"))
 
-   (define *tempdir* (get-environment-variable "TEMP"))
-   (define *defaultdir* "c:\\program files (x86)\\steam\\steamapps\\sourcemods")
+    (define *tempdir* (get-environment-variable "TEMP"))
+    (define *defaultdir* "c:\\program files (x86)\\steam\\steamapps\\sourcemods")
 
-   (define *theme* "xpnative")
-   (define *progbarsize* 645))
+    (define *theme* "xpnative")
+    (define *progbarsize* 645))
 
   (linux
     (define *tempdir* (make-absolute-pathname "var" "tmp"))
@@ -44,25 +44,17 @@
     (define *theme* "clam")
     (define *progbarsize* 565)))
 
-(define *ariaargs*
+(define *curlargs*
   (list
-    "--enable-color=false"
-    "-x 16"
-    "-UTF2CDownloadergui2024-11-24"
-    "--allow-piece-length-change=true"
-    "-j 16"
-    "--optimize-concurrent-downloads=true"
-    "--check-certificate=false"
-    "-V"
-    "--auto-file-renaming=false"
-    "-c"
-    "--allow-overwrite=true"
-    "--console-log-level=error"
-    "--summary-interval=5"
-    "--bt-hash-check-seed=false"
-    "--seed-time=0"
-    "-d"
-    *tempdir*))
+;    "-#"
+    "-A tf2cd-gui20241001"
+    "-C" "-"
+    "-O"
+    "--output-dir"
+    *tempdir*
+    ;"--http3-only"
+    "--stderr" "-"
+    ))
 
 ; we append multiple args to some of these later
 (define *unpackargs* (list "-xvf"))
@@ -213,7 +205,7 @@
 	  (statusstate 0))))))
 
 (define (installproc)
-  (let*-values ([(rid) (tk-get-var 'userdir)] [(a b c) (process *downloader* (append *ariaargs* (list *fulltarballurl*)))])
+  (let*-values ([(rid) (tk-get-var 'userdir)] [(a b c) (process *curl* (append *curlargs* (list *fulltarballurl*)))])
     (begin
       (buttonstate 0)
       (statusstate 1)
